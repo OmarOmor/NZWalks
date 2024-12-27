852e5caef25eee738d0a4c0b9872e855c95331cf
OmarOmor <59320555+OmarOmor@users.noreply.github.com>
2024-12-28 01:47:23 +0600

    Completed adding DTOs for RegionController

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Data;
 using NZWalks.API.Models.Domain;
 using NZWalks.API.Models.DTOs;
+using System.Linq.Expressions;
 
 namespace NZWalks.API.Controllers
 {
@@ -19,7 +20,6 @@ namespace NZWalks.API.Controllers
 
 
         [HttpGet]
-
         public IActionResult GetAll()
         {
 
@@ -64,5 +64,95 @@ namespace NZWalks.API.Controllers
             return Ok(regionDTO);
 
         }
+
+
+        [HttpPost]
+        public IActionResult Create([FromBody]AddRegionRequestDTO addRegionRequestDTO)
+        {
+            var regionDomain = new Region
+            {
+                Code = addRegionRequestDTO.Code,
+                Name = addRegionRequestDTO.Name,
+                RegionImageURL = addRegionRequestDTO.RegionImageURL,
+            };
+
+            _context.Regions.Add(regionDomain);
+            _context.SaveChanges();
+
+            var regionDTO = new RegionDTO
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageURL = regionDomain.RegionImageURL,
+            };
+
+
+            return CreatedAtAction(nameof(GetById),new {id = regionDTO.Id },regionDTO);
+        }
+
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public IActionResult Update([FromRoute] Guid id, [FromBody]UpdateRegionRequestDTO updateRegionRequestDTO)
+        {
+            var regionDomain = _context.Regions.FirstOrDefault(x => x.Id == id);
+            if(regionDomain == null)
+            {
+                return NotFound();
+            }
+
+            //Update the required data
+            regionDomain.Code = updateRegionRequestDTO.Code;
+            regionDomain.Name = updateRegionRequestDTO.Name;
+            regionDomain.RegionImageURL = updateRegionRequestDTO.RegionImageURL;
+
+            _context.SaveChanges();
+
+
+            var regionDTO = new RegionDTO
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageURL = regionDomain.RegionImageURL,
+            };
+
+
+            return Ok(regionDTO);
+        }
+
+
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            var regionDomain = _context.Regions.FirstOrDefault(x => x.Id == id);
+
+            if( regionDomain == null)
+            {
+                return NotFound();
+            }
+
+            _context.Regions.Remove(regionDomain);
+            _context.SaveChanges();
+
+
+            var regionDTO = new RegionDTO
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageURL = regionDomain.RegionImageURL,
+            };
+
+            return Ok(regionDTO);
+
+        }
     }
+
+
+
 }
