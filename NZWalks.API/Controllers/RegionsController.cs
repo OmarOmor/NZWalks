b6df0871588c30dd9679bc013c51d492cf67423a
OmarOmor <59320555+OmarOmor@users.noreply.github.com>
2024-12-27 16:45:10 +0600

    Add project files.

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland",
+                    Code = "AL",
+                    RegionImageURL = "https://images.pexels.com/photos/2072264/pexels-photo-2072264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Buckland",
+                    Code = "BL",
+                    RegionImageURL = "https://images.pexels.com/photos/2072264/pexels-photo-2072264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+
+                }
+            };
+
+            return Ok(regions);
+        }
+    }
+}
